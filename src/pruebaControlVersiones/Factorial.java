2b312e918698b6ec43553ceb13d7eae4a9a509e0
aveggar521 <aveggar521@g.educaand.es>
2025-12-10 08:57:24 +0100

    realizada la funcion para pedir numero en la version b

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Factorial {
 
+	private static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Introduce un número entero mayor o igual que 0: ");
 		int num = sc.nextInt();
@@ -15,8 +16,13 @@ public class Factorial {
 		} else {
 			System.out.println("Has introducido el número: " + num);
 		}
-
+		System.out.println(pedirNumero());
 		sc.close();
+	}
+
+	private static int pedirNumero() {
+		System.out.print("Introduce un número entero mayor o igual que 0: ");
+		return sc.nextInt();
 	}
 
 }
